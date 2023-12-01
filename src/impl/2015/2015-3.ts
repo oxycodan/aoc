2c2850e7c6e98aab19ed95717db46e5feb970ba5
hxruhime <38597755+hxruhime@users.noreply.github.com>
2023-12-01 15:24:15 -0800

    update file naming + start 2015-3

---
+++ src/impl/2015/2015-3.ts
@@ -0,0 +1,24 @@
+import Day from "@src/day";
+
+export class DayThree extends Day {
+
+    readonly partOneExample       : string = '';
+    readonly partTwoExample       : string = '';
+
+    readonly partOneExampleAnswer : string = '';
+    readonly partTwoExampleAnswer : string = '';
+
+    constructor() { super(2015, 3); }
+
+    partOne(input: string): string {
+
+        return '0';
+    }
+
+    partTwo(input: string): string {
+
+        return '0';
+    }
+}
+
+export default new DayThree;
